c40d6a5b4bb5dc92520953e603632a81cd2b4efd
linzhicai <linzhicai@dobest.com>
2024-12-31 15:08:13 +0800

    fix(取消console)

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 
 import preloadDirective from '@/directives/preload';
-import VConsole from 'vconsole';
 import { createApp } from 'vue';
 import App from './App.vue';
 import './style.css';
-const vConsole = new VConsole();
+// const vConsole = new VConsole();
 
 
 createApp(App).directive('preload', preloadDirective).mount("#app");
